356b9c4f4be2ccfb7deeb561b541e53406296584
Lazernes <dbsauddn1@gmail.com>
2026-04-23 12:51:20 +0900

    [Init] Initial Setting

---
+++ src/test/java/com/finlearn/seasonservice/SeasonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finlearn.seasonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
